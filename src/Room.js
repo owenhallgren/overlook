2233a11563cc49f5dffc2f084b0302a901d80e76
Owen Hallgren <owenhallgren@gmail.com>
2021-03-04 16:38:38 -0700

    Added room properties

---
+++ src/Room.js
@@ -0,0 +1,12 @@
+
+class Room {
+  constructor(roomObj) {
+    this.number = roomObj.number;
+    this.roomType = roomObj.roomType;
+    this.bidet = roomObj.bidet;
+    this.bedSize = roomObj.bedSize;
+    this.numBeds = roomObj.numBeds;
+    this.costPerNight = roomObj.costPerNight;
+  }
+
+}
