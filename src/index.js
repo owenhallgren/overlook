f3c6e1330eccb56150753cae4d2908e33ec7423a
Owen Hallgren <owenhallgren@gmail.com>
2021-03-09 13:01:57 -0700

    Added error handling

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import User from './User';
 let userData = [];
 let roomData = [];
 let bookedRoomData = [];
-let moneySpent = document.querySelector(".money-spent-test");
+let moneySpent = document.querySelector(".money-spent");
 let bookRoomButton = document.querySelector(".book-room")
+
 
 
 function fetchData() {
@@ -28,16 +29,60 @@ window.addEventListener('load', fetchData)
 
 function initialize (userData, roomData, bookedRoomData) {
   let user = new User(userData[0], bookedRoomData, roomData);
-  let moneySpent = document.querySelector(".money-spent-test");
-  let bookRoomButton = document.querySelector(".book-room")
+  let moneySpent = document.querySelector(".money-spent");
+  // let bookRoomButton = document.querySelector(".book-room")
   let roomsBookedTable = document.querySelector(".rooms-booked-table")
   let tableBody = document.querySelector(".table-body");
   let openRoomsTableBody = document.querySelector('.open-rooms-body')
+  // let bookRoomButton = document.querySelector(".book-room-button")
+  let dateInput = document.querySelector(".date-input")
+  let findRoomButton = document.querySelector(".find-room")
+  let successfulSubmit = document.querySelector(".successful-submit")
+  let failedSubmit = document.querySelector(".failed-submit")
+  let showBookedRoomSection = document.querySelector('.show-book-room-section')
+  let bookRoomSection = document.querySelector(".book-room-section")
+  let homePage = document.querySelector(".home-page")
+  let homeButton = document.querySelector(".go-home-button")
+
+
+
+
+  findRoomButton.addEventListener('click', findOpenRoomHelper)
+  showBookedRoomSection.addEventListener('click', showBookRoomSection)
+  homeButton.addEventListener("click", hideBookRoomSection)
+
+  function showBookRoomSection() {
+    bookRoomSection.classList.remove('hidden')
+    homePage.classList.add("hidden")
+    homeButton.classList.remove("hidden")
+    showBookedRoomSection.classList.add('hidden')
+  }
+
+  function hideBookRoomSection() {
+    bookRoomSection.classList.add('hidden')
+    homePage.classList.remove("hidden")
+    showBookedRoomSection.classList.remove('hidden')
+    homeButton.classList.add("hidden")
+    successfulSubmit.classList.add("hidden")
+    failedSubmit.classList.add("hidden")
+  }
+
+  function bookSelectedRoom(e) {
+    let roomNumber = parseInt(e.target.getAttribute("data-room"))
+    let matchedRoom = roomData.find(room => room.number === roomNumber)
+    user.bookRoom(matchedRoom)
+    let date = dateInput.value.replaceAll("-", "/")
+    postBooking(date, roomNumber)
+  }
 
 
 
 displayPrice();
 displayBookedRooms();
+
+function findOpenRoomHelper() {
+  findOpenRooms(dateInput.value.replaceAll("-", "/"))
+}
 
 
 function displayBookedRooms() {
@@ -55,9 +100,12 @@ function displayBookedRooms() {
     `
   }) }
 
-  findOpenRooms("2020/01/25")
+
+
+
 
   function findOpenRooms(date) {
+
     let filteredByDate = bookedRoomData.filter(room => date === room.date )
     let roomsToDisplay = filteredByDate.map(bookedRoom => roomData.find(room => room.number === bookedRoom.roomNumber))
 
@@ -70,19 +118,47 @@ function displayBookedRooms() {
         <td>${room.bedSize}</td>
         <td>${room.bidet? 'Yes':''}</td>
         <td class="right-alligned">${room.costPerNight}</td>
+        <td><button data-room="${room.number}" class="book-room-button">Book Room</button></td>
       </tr>
 
       `
     })
+    const bookedRoomButtons = document.querySelectorAll('.book-room-button')
+    bookedRoomButtons.forEach(function(currentBtn){
+      currentBtn.addEventListener('click', bookSelectedRoom)
+    })
+
+
   }
 
 
 
 function displayPrice() {
-  // console.log(user.roomsBooked)
   moneySpent.innerText = `${user.calculateTotalPrice()}`
 }
 
-// test()
+function postBooking(date, roomNumber) {
+  fetch(`http://localhost:3001/api/v1/bookings`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({"userID": user.id, "date": date, "roomNumber": roomNumber})
+  })
+  .then(checkForError)
+  .catch(err => showErrorMessage());
+}
 
+const checkForError = response => {
+  if (!response.ok) {
+    showErrorMessage();
+  } else {
+    successfulSubmit.classList.remove("hidden")
+    return response.json();
+  }
+}
+
+function showErrorMessage() {
+  failedSubmit.classList.remove("hidden")
+}
 }
